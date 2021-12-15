facab70f7d60cd0f2123343b91edd90425752e13
Austin Tran <vu@floatingcube.com>
2021-12-15 08:59:13 +0700

    First commit

---
+++ lua/mappings.lua
@@ -0,0 +1,64 @@
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+vim.g.mapleader = ","
+
+-- Nvim Tree
+map("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
+map("n", "<leader>u", ":nvimtreefindfile<cr>", { silent = true })
+
+-- Telescope
+map("n", "<Leader>1", ":Telescope sessions [save_current=true]<CR>")
+map("n", "<leader>p", '<cmd>lua require("telescope.builtin").find_files()<cr>')
+map("n", "<leader>r", '<cmd>lua require("telescope.builtin").registers()<cr>')
+map("n", "<leader>g", '<cmd>lua require("telescope.builtin").live_grep()<cr>')
+map("n", "<leader>b", '<cmd>lua require("telescope.builtin").buffers()<cr>')
+map("n", "<leader>j", '<cmd>lua require("telescope.builtin").help_tags()<cr>')
+map("n", "<leader>h", '<cmd>lua require("telescope.builtin").git_bcommits()<cr>')
+map("n", "<leader>f", '<cmd>lua require("telescope").extensions.file_browser.file_browser()<CR>')
+map("n", "<leader>s", '<cmd>lua require("telescope.builtin").spell_suggest()<cr>')
+map("n", "<leader>i", '<cmd>lua require("telescope.builtin").git_status()<cr>')
+map("n", "<leader>ca", '<cmd>lua require("telescope.builtin").lsp_code_actions()<cr>')
+map("n", "<leader>cs", '<cmd>lua require("telescope.builtin").lsp_document_symbols()<cr>')
+map("n", "<leader>cd", '<cmd>lua require("telescope.builtin").lsp_document_diagnostics()<cr>')
+map("n", "<leader>cr", '<cmd>lua require("telescope.builtin").lsp_references()<cr>')
+
+-- LSP config
+map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+map("n", "]d", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
+map("n", "[d", "<cmd>lua vim.diagnostic.goto_next()<CR>")
+map("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>")
+
+--After searching, pressing escape stops the highlight
+map("n", "<esc>", ":noh<cr><esc>", { silent = true })
+
+-- Easy select all of file
+map("n", "<Leader>sa", "ggVG<c-$>")
+
+-- Line bubbling
+map("n", "<c-j>", "<cmd>m .+1<CR>==", { silent = true })
+map("n", "<c-k>", "<cmd>m .-2<CR>==", { silent = true })
+map("v", "<c-j>", ":m '>+1<CR>==gv=gv", { silent = true })
+map("v", "<c-k>", ":m '<-2<CR>==gv=gv", { silent = true })
+
+-- Easier split mappings
+
+-- Rename
+map("i", "<F2>", '<cmd>lua require("renamer").rename()<cr>', { noremap = true, silent = true })
+map("n", "<leader>nr", '<cmd>lua require("renamer").rename()<cr>', { noremap = true, silent = true })
+
+-- Hop
+map("n", "nf", "<cmd>lua require'hop'.hint_char1()<cr>")
+map("n", "ng", "<cmd>lua require'hop'.hint_char2()<cr>")
+map("n", "nl", "<cmd>lua require'hop'.hint_lines()<cr>")
+
+-- Symbols outline
+map("n", "<leader>o", ":SymbolsOutline<cr>")
---
+++ lua/setup/null-ls.lua
@@ -0,0 +1,31 @@
+-- Here is the formatting config
+local null_ls = require("null-ls")
+local lSsources = {
+  null_ls.builtins.formatting.prettier.with({
+    filetypes = {
+      "javascript",
+      "typescript",
+      "ruby",
+      "go",
+      "css",
+      "scss",
+      "html",
+      "json",
+      "yaml",
+      "markdown",
+      "graphql",
+      "md",
+      "txt",
+    },
+  }),
+  null_ls.builtins.formatting.stylua.with({
+    args = { "--indent-width", "2", "--indent-type", "Spaces", "-" },
+  }),
+}
+require("null-ls").config({
+  sources = lSsources,
+})
+require("lspconfig")["null-ls"].setup({})
+-- the duration in there is to stop timeouts on massive files
+vim.cmd("autocmd BufWritePost * lua vim.lsp.buf.formatting_seq_sync(nil, 7500)")
+vim.o.updatetime = 250

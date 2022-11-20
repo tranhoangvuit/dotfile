6a374d2baae854c701000040559283c84e6ecb5e
Austin Tran <tranhoangvu.it1990@gmail.com>
2022-11-20 23:22:28 +0700

    Remove incorrect config for mason

--- after/plugin/mason.rc.lua
+++ after/plugin/mason.rc.lua
@@ -4,11 +4,10 @@ local status2, lspconfig = pcall(require, "mason-lspconfig")
 if (not status2) then return end
 
 mason.setup({
-  ensure_installed = { "sumneko_lua", "rust_analyzer", "gopls", "golangci_lint_ls" }
+  ensure_installed = { "sumneko_lua", "rust_analyzer", "gopls" }
 })
 
 require("lspconfig").gopls.setup {}
-require("lspconfig").golangci_lint_ls.setup {}
 
 lspconfig.setup {
   automatic_installation = true

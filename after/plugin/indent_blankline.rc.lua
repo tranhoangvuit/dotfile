25b4f73c49e14202104cfe627deb30ece7383eaf
Austin Tran <tranhoangvu.it1990@gmail.com>
2022-12-09 23:21:14 +0700

    Update new theme kanagawa

--- after/plugin/indent_blankline.rc.lua
+++ after/plugin/indent_blankline.rc.lua
@@ -3,15 +3,15 @@ if (not status) then return end
 
 vim.opt.list = true
 -- vim.opt.termguicolors = true
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#586e75 gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#586e75 gui=nocombine]]
 -- vim.opt.listchars:append "space:⋅"
 
 indent_blankline.setup({
   require("indent_blankline").setup {
     -- show_end_of_line = true,
     space_char_blankline = " ",
-    char_highlight_list = {
-      "IndentBlanklineIndent1",
-    },
+    -- char_highlight_list = {
+    --   "IndentBlanklineIndent1",
+    -- },
   }
 })

5e0e897caf9605c4d9aabfd2bd25f36e4d0a31a7
Austin Tran <tranhoangvu.it1990@gmail.com>
2023-10-03 19:07:42 +0700

    chore: set short key for neotree reveal

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -10,6 +10,20 @@ return {
     -- Unless you are still migrating, remove the deprecated commands from v1.x
     vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
     vim.keymap.set('n', '<C-e>', '<Cmd>Neotree toggle<CR>')
-    require('neo-tree').setup {}
+    require('neo-tree').setup({
+      filesystem = {
+        follow_current_file = {
+          enable = true,
+          leave_dirs_open = false,
+        }
+      },
+      buffers = {
+        follow_current_file = {
+          enabled = true,          -- This will find and focus the file in the active buffer every time
+          leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+        },
+      }
+    })
+    vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
   end,
 }
